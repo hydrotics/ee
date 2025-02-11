24e589dbd7704941400fa9a88ffe2643b6a0577a
hydrotics <hydrotestacc@gmail.com>
2025-02-11 20:06:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ import difflib
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+
 keep_alive()
 
 # Load environment variables
@@ -149,22 +165,6 @@ async def on_message(message):
                 await message.channel.send(response)
 
     await bot.process_commands(message)
-
-from flask import Flask
-from threading import Thread
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "I'm alive!"
-
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
 
 # Run the bot
 bot.run(TOKEN)
