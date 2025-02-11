46912165e4ba885f18acc480d008ada6a6970941
hydrotics <hydrotestacc@gmail.com>
2025-02-11 18:54:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -148,5 +148,21 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+
 # Run the bot
 bot.run(TOKEN)
